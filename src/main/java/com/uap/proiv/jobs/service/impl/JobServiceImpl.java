326964a46ee350e2869ab5bd5bae1960970fdf0b
Said Quioto <said.quioto@uap.edu.ar>
2026-08-13 20:10:48 -0300

    Se sube el test de la clase UserJobAssignedServiceImplTest.java

--- src/main/java/com/uap/proiv/jobs/service/impl/JobServiceImpl.java
+++ src/main/java/com/uap/proiv/jobs/service/impl/JobServiceImpl.java
@@ -6,6 +6,7 @@ import com.uap.proiv.jobs.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.beans.Transient;
 import java.util.List;
 
 @Service
